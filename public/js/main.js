b530b4700e73bf0cf243102a921b0c3839ec4b29
odilonjk <kroger.odilonj@gmail.com>
2017-05-27 14:30:08 -0300

    Adicionado cronometro e bloqueio do textArea

--- public/js/main.js
+++ public/js/main.js
@@ -10,11 +10,29 @@ tamanhoFrase.text(numPalavras);
 
 //  Obter textArea
 var campo = $(".campo-digitacao");
-campo.on("keyup", function() {
-
+campo.on("input", function() {
   // Contando caracteres e adicionando ao contador.
   $("#contador-caracteres").text(campo.val().length);
 
   // Contando palavras e adicionando ao contador.
   $("#contador-palavras").text(campo.val().split(/\S+/).length -1);
+});
+
+//  Fazer a redução do tempo de jogo
+var tempoRestante = $("#contador-segundos").text();
+campo.one("focus", function () {
+  //  Setado para subtrair a cada 1000ms
+  var cronometro = setInterval(function () {
+    tempoRestante--;
+    //  Atribuindo o novo valor
+    $("#contador-segundos").text(tempoRestante);
+
+    //  Desabilitando textArea ao fim do tempo
+    if(tempoRestante < 1) {
+      campo.attr("disabled", true);
+
+      //  Desligando o cronometro
+      clearInterval(cronometro);
+    }
+  }, 1000);
 });
