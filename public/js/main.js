9ae5fb37fdad980b84b2b5b677b392299271b183
odilonjk <kroger.odilonj@gmail.com>
2017-05-27 11:47:35 -0300

    Adicionado textArea e contadores

--- public/js/main.js
+++ public/js/main.js
@@ -5,5 +5,16 @@ var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 
 //  Obter contador de palavras da lista e setar a quantia correta
-var contadorPalavras = $("#contador-palavras");
-contadorPalavras.text(numPalavras);
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
+
+//  Obter textArea
+var campo = $(".campo-digitacao");
+campo.on("keyup", function() {
+
+  // Contando caracteres e adicionando ao contador.
+  $("#contador-caracteres").text(campo.val().length);
+
+  // Contando palavras e adicionando ao contador.
+  $("#contador-palavras").text(campo.val().split(" ").length);
+});
