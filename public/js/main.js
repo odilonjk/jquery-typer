6ebce7a3d0c9f41751a21c97c1eb3688657e859f
odilonjk <kroger.odilonj@gmail.com>
2017-05-27 15:07:33 -0300

    Adicionado botão reiniciar e refatoração do codigo

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,66 @@
-//  Obter tamanho da frase.
-//  Em vez de usar o document.querySelector
-//  podemos usar o jQuery ( jQuery() = $() )
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
+//  Obter tempo inicial
+var tempoInicial = $("#contador-segundos").text();
+var campo = $(".campo-digitacao");
+
+$(function () {
+  atualizaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  $("#botao-reiniciar").click(reiniciarJogo);
+});
 
 //  Obter contador de palavras da lista e setar a quantia correta
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
+function atualizaTamanhoFrase() {
+  //  Obter tamanho da frase.
+  //  Em vez de usar o document.querySelector
+  //  podemos usar o jQuery ( jQuery() = $() )
+  var frase = $(".frase").text();
+  var numPalavras = frase.split(" ").length;
 
-//  Obter textArea
-var campo = $(".campo-digitacao");
-campo.on("input", function() {
-  // Contando caracteres e adicionando ao contador.
-  $("#contador-caracteres").text(campo.val().length);
+  var tamanhoFrase = $("#tamanho-frase");
+  tamanhoFrase.text(numPalavras);
+};
 
-  // Contando palavras e adicionando ao contador.
-  $("#contador-palavras").text(campo.val().split(/\S+/).length -1);
-});
+function inicializaContadores() {
+  //  Obter textArea
+  campo.on("input", function() {
+    // Contando caracteres e adicionando ao contador.
+    $("#contador-caracteres").text(campo.val().length);
+
+    // Contando palavras e adicionando ao contador.
+    $("#contador-palavras").text(campo.val().split(/\S+/).length -1);
+  });
+};
 
 //  Fazer a redução do tempo de jogo
-var tempoRestante = $("#contador-segundos").text();
-campo.one("focus", function () {
-  //  Setado para subtrair a cada 1000ms
-  var cronometro = setInterval(function () {
-    tempoRestante--;
-    //  Atribuindo o novo valor
-    $("#contador-segundos").text(tempoRestante);
+function inicializaCronometro() {
+  var tempoRestante = $("#contador-segundos").text();
+  campo.one("focus", function () {
+    //  Setado para subtrair a cada 1000ms
+    var cronometro = setInterval(function () {
+      tempoRestante--;
+      //  Atribuindo o novo valor
+      $("#contador-segundos").text(tempoRestante);
 
-    //  Desabilitando textArea ao fim do tempo
-    if(tempoRestante < 1) {
-      campo.attr("disabled", true);
+      //  Desabilitando textArea ao fim do tempo
+      if(tempoRestante < 1) {
+        campo.attr("disabled", true);
 
-      //  Desligando o cronometro
-      clearInterval(cronometro);
-    }
-  }, 1000);
-});
+        //  Desligando o cronometro
+        clearInterval(cronometro);
+      }
+    }, 1000);
+  });
+};
+
+function reiniciarJogo() {
+  //  Reiniciar campo
+  campo.attr("disabled", false);
+  campo.val("");
+
+  //  Reiniciar contadores
+  $("#contador-segundos").text(tempoInicial);
+  $("#contador-palavras").text("0");
+  $("#contador-caracteres").text("0");
+  inicializaCronometro();
+}
