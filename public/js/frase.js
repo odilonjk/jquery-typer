6817e145f76b5f849eb5ba0eb68f0d94fa948597
odilonjk <kroger.odilonj@gmail.com>
2017-06-19 22:09:48 -0300

    Adicionado spinner para mostrar ao fazer request no servidor

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,14 +1,20 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
+
+  $("#spinner").show();
+
   $.get("http://dockerhost:3000/frases", trocaFrase)
     .fail(function () {
         $("#erro").toggle();
         setTimeout(function () {
           $("#erro").toggle();
         }, 2500)
-      }
-    )
+    })
+    .always(function () {
+      $("#spinner").hide();
+    })
+
 };
 
 function trocaFrase(data) {
