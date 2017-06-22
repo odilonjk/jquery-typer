ae79495298d6162de737906c8ee0f9d9be8491a7
odilonjk <kroger.odilonj@gmail.com>
2017-06-22 20:46:25 -0300

    Adicionado selecionar frase por ID

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,11 +1,12 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
 
   $("#spinner").show();
 
-  $.get("http://dockerhost:3000/frases", trocaFrase)
-    .fail(function () {
+  $.get("http://dockerhost:3000/frases", trocaFraseRandom)
+    .fail(function() {
         $("#erro").toggle();
         setTimeout(function () {
           $("#erro").toggle();
@@ -17,7 +18,31 @@ function fraseAleatoria() {
 
 };
 
+function buscaFrase() {
+  $("#spinner").show();
+
+  var id = $("#frase-id").val();
+  var dados = { id: id};
+  $.get("http://dockerhost:3000/frases", dados, trocaFrase)
+  .fail(function() {
+    $("#erro").toggle();
+    setTimeout(function () {
+      $("#erro").toggle();
+    }, 2500)
+  })
+  .always(function () {
+    $("#spinner").hide();
+  })
+};
+
 function trocaFrase(data) {
+  var frase = $(".frase");
+  frase.text(data.texto);
+  atualizaTamanhoFrase();
+  atualizaTempoInicial(data.tempo);
+}
+
+function trocaFraseRandom(data) {
   var frase = $(".frase");
   var indice = Math.floor(Math.random() * data.length);
   frase.text(data[indice].texto);
